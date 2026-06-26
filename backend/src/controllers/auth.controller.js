e0044eab6b8adf4a85932314ffa62c745a5c3d02
Sarvam Taneja <sarvamtaneja@gmail.com>
2026-06-27 02:25:30 +0530

    add register and login routes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,80 @@
-const userModel = require("../models/user.model")
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 
-function userRegisterController(req,res){
-    
+/**
+ * - user register controller
+ * - POST /api/auth/register
+ */
+async function userRegisterController(req,res){
+    const {email, password, name} = req.body;
+
+    const doesUserExist = await userModel.findOne({
+        email:email
+    })
+
+    if(doesUserExist){
+        return res.status(402).json({
+            message:"User already exists with email",
+            status:"failed"
+        })
+    }
+
+    const user = await userModel.create({
+        email, password, name
+    })
+
+    const token = jwt.sign({userId:user._id},process.env.JWT_SECRET, {expiresIn:"3d"});
+
+    res.cookie("token", token);
+
+    res.status(201).json({
+        user:{
+            _id: user._id,
+            email: user.email,
+            name: user.name
+        },
+        token
+    })
 }
+
+/**
+ * - user Login controller
+ * - POST /api/auth/login 
+ */
+async function userLoginController(req,res){
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email }).select("+password");
+
+    if(!user){
+        return res.staus(401).json({
+            message: "email or password is INVALID"
+        })
+    }
+
+    const isValidPassword = await user.comparePassword(password);
+
+    if(!isValidPassword){
+        return res.status(401).json({
+            message:"email or password is INVALID"
+        })
+    }
+
+    const token = jwt.sign({ userId: user._id}, process.env.JWT_SECRET, {expiresIn: "3d"});
+
+    res.cookie("token",token);
+
+    res.status(200).json({
+        user:{
+            _id: user._id,
+            email: user.email,
+            name: user.name
+        },
+        token
+    });
+
+}
+module.exports = {
+    userRegisterController,
+    userLoginController
+};
