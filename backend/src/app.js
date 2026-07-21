ec6b999741bff095f64cd43bff4151399636255a
Sarvam Taneja <sarvamtaneja@gmail.com>
2026-07-21 15:02:41 +0530

    add frontend dependencies

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,11 +4,14 @@ const cookieParser =  require("cookie-parser");
 const authRouter = require("./routes/auth.routes");
 const accountRouter =  require("./routes/account.routes");
 const transactionRouter = require("./routes/transaction.routes");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.get("/", (req,res)=>{
     res.send("Banking service is up and running");
